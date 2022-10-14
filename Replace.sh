9c4f58028becbce68f25b2513c960d8ceac1d2b2
lihaibin <lihaibin@daojia-inc.com>
2022-10-14 14:10:27 +0800

    命令执行参数

--- Replace.sh
+++ Replace.sh
@@ -3,5 +3,9 @@ str='"tag/opqen-platpyform_dowyf-community-partner_8-0-83-0_dajtoy"'
 str=${str/\"/}
 str=${str/\"/}
 echo $str
+
+#如何将echo命令执行的参数赋值给变量 使用 `` 命令
+#var=`command`
+#var=$(command)
 s2=$(echo $str | sed 's/"//g')
 echo $s2
